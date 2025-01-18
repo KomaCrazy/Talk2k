38bf26622ea9b78ef6265cdcab81b25c266a9e66
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2025-01-18 19:23:22 +0700

    Add pkg http

---
+++ API/cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+	r.Use(gin.Logger())
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":80")
+}
